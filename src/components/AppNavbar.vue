0fbf9f55f731489a2e1637b0968575e3c6091d90
Surajit05021997 <surajitmaity12345@gmail.com>
2021-11-03 17:18:33 +0530

    Initial Commit

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <a href="#" class="logo">Wallet-Insurance</a>
+      <a href="#" class="login-btn">Login</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .navbar {
+    background-color: #007fff;
+    padding: 1.5rem 2rem;
+  }
+
+  .container {
+    max-width: 120rem;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .logo:link, .logo:visited {
+    font-size: 2.5rem;
+    color: #fff;
+    text-decoration: none;
+  }
+
+  .logo:hover, logo:active {
+    cursor: pointer;
+  }
+
+  .login-btn:link, .login-btn:visited {
+    font-size: 1.5rem;
+    color: #333;
+    text-decoration: none;
+    padding:0.5rem 1rem;
+    background-color: #fff;
+    border-radius: 20rem;
+    transition: all 0.3s;
+  }
+
+  .login-btn:hover, login-btn:active {
+    cursor: pointer;
+    box-shadow: inset 0 0 0 0.2rem #fff;
+    background-color: #007fff;
+    color: #fff;
+  }
+</style>
